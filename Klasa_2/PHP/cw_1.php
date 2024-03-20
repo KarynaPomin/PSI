b449b04a2ef34a5679900eccb68bb62bea13a244
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-03-20 12:39:10 +0100

    Update cw_1.php

--- Klasa_2/PHP/cw_1.php
+++ Klasa_2/PHP/cw_1.php
@@ -1,25 +1,25 @@
 <?PHP
     // http://localhost/2E/PAI/cw_1.php
 
-    // echo('Hello!');
-    // print('Hello');
-    // phpinfo();
+    echo('Hello!');
+    print('Hello');
+    phpinfo();
 
-    // $imie = 'Karyna';
-    // $nazwisko = 'Pomin';
-    // print('Nazywam się <i>'. $imie .' '. $nazwisko . "</i><br>");
+    $imie = 'Karyna';
+    $nazwisko = 'Pomin';
+    print('Nazywam się <i>'. $imie .' '. $nazwisko . "</i><br>");
     
-    // $bokA = 5;
-    // $bokB = 7;
-    // print('<u>Pole</u> prostokąta o bokach: A = '. $bokA .', B = '. $bokB . ' wynosi: <b>' .  $bokA * $bokB . '</b><br>');
-    // print('<u>Obwód</u> prostokąta o bokach: A = '. $bokA .', B = '. $bokB . ' wynosi: <b>' .  $bokA + $bokB . '</b><br>');
+    $bokA = 5;
+    $bokB = 7;
+    print('<u>Pole</u> prostokąta o bokach: A = '. $bokA .', B = '. $bokB . ' wynosi: <b>' .  $bokA * $bokB . '</b><br>');
+    print('<u>Obwód</u> prostokąta o bokach: A = '. $bokA .', B = '. $bokB . ' wynosi: <b>' .  $bokA + $bokB . '</b><br>');
 
-    // define ("KWOTA_DO_ODJĘCIA", 436.20);
-    // define ("STAWKA_PODATKOWA", 0.19); // czyli 19%
-    // print ("Podtek od dochodu 5000 PLN w 2000 roku wynosi: ");
+    define ("KWOTA_DO_ODJĘCIA", 436.20);
+    define ("STAWKA_PODATKOWA", 0.19); // czyli 19%
+    print ("Podtek od dochodu 5000 PLN w 2000 roku wynosi: ");
     
-    // printF ("%0.2f", (5000 * STAWKA_PODATKOWA - KWOTA_DO_ODJĘCIA)); // "%0.2" --> dokładność do 2-ch miejsc po przecinku
-    // print (" PLN");
+    printF ("%0.2f", (5000 * STAWKA_PODATKOWA - KWOTA_DO_ODJĘCIA)); // "%0.2" --> dokładność do 2-ch miejsc po przecinku
+    print (" PLN");
 
     $tekst = 'Radek';
     $tekst .= 'Barski';
